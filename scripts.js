b7490af1f24beec8ad220f5727e00a79422f596f
krishusmalicious <krishusmalicious@gmail.com>
2025-07-31 17:53:05 +0800

    whats a commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const downloadButton = document.getElementById('downloadButton');
+    downloadButton.addEventListener('click', () => {
+        alert('Your download for skibidiscript.zip is starting!');
+    });
+});
